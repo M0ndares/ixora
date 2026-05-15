59f0064250a4370232c6f3376138ed045165f6cd
Osval Montesinos <omontesinos@ucol.mx>
2026-05-15 11:07:14 -0600

    API REST añadido

--- back/1/app.js
+++ back/1/app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-app.use(express.json())
+const PORT = 3000;
+// Aquí debemos importar las rutas que definamos
+const estudiantesRoutes = require('./routes/estudiantesRoutes');
+const testRoutes = require('./routes/testRoutes');  // Ruta de prueba
+app.use(express.json());
 
-app.get('/si', (req, res) => {
-    res.send('no');
-});
+app.use('/prueba', (req,res) => {
+   res.send('Esto es un ruta de prueba, puedo definir una ruta o algo aquí');
+});  
+app.use('/api', estudiantesRoutes);
 
-app.listen(port, () => {
-    console.log(`Escuchando mediante http://localhost:${port}`)
+
+
+
+//probar la conexión a la base de datos
+app.use('/test', testRoutes);
+
+//Respuesta con formato con html tiene que ir al final por que si no se ejecuta primero y no tienen acceso a las rutas
+app.use((req, res) => {
+  res.status(404).send('<h1>Error 404</h1><p>La ruta que intentas acceder no existe.</p>');
+})
+
+
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
---
+++ back/1/config/db.js
@@ -0,0 +1,22 @@
+
+const mysql = require('mysql2');
+const conexion= mysql.createConnection({
+  host: 'localhost',   
+  user: 'root', // Cambia esto por tu usuario de MySQL
+  password: 'oeoe1221', // Cambia esto por tu contraseña de MySQL
+  database: 'example', // Cambia esto por el nombre de tu base de datos
+  port: 3306   //revisar el puerto, por defecto es 3306
+});
+
+// Probar la conexión
+
+
+conexion.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a MySQL:', err);
+    return;
+  }
+  console.log('Conectado a MySQL.');
+});
+
+module.exports = conexion
