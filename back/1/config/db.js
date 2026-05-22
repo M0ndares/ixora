121ffc7569ac02c5b764022b8124fa2b5deea104
Ashley Loza Velazquez <aloza1@ucol.mx>
2026-05-22 07:18:56 -0600

    hola

--- back/1/config/db.js
+++ back/1/config/db.js
@@ -3,7 +3,7 @@ const conexion= mysql.createConnection({
   host: 'localhost',   
   user: 'root', // Cambia esto por tu usuario de MySQL
   password: '1234', // Cambia esto por tu contraseña de MySQL
-  database: 'ixora', // Cambia esto por el nombre de tu base de datos
+  database: 'sistema_hotel', // Cambia esto por el nombre de tu base de datos
   port: 3306   //revisar el puerto, por defecto es 3306
 });
 
